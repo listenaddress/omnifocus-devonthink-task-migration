c94a5a588fa2850e6c24c8629c5b307cf241fcd9
listenaddress <tbobrien612@gmail.com>
2021-09-09 21:41:13 -0400

    Create DEVONThink doc for each OmniFocus Project

--- construct-html.js
+++ construct-html.js
@@ -1,7 +1,6 @@
 const fs = require('fs')
 const convert = require('xml-js')
 
-let html = ''
 const data = fs.readFileSync('contents.xml')
 const jsonString = convert.xml2json(data)
 const json = JSON.parse(jsonString)
@@ -13,33 +12,33 @@ const topLevelTasks = tasks.filter(t => {
   }
 })
 
-const ammendHeader = () => {
-  html += '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta name="DT:isEditableNote" content="Yes"><style></style></head>'
+const ammendHeader = (html) => {
+  return html += '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta name="DT:isEditableNote" content="Yes"><style></style></head>'
 }
-const ammendTopLevelTask = (task) => {
-  const name = task.elements.find(e => e.name === 'name' && e.elements.length === 1)
-  html += `<body style="margin: 1.5em; word-wrap: break-word; -webkit-nbsp-mode: space; line-break: after-white-space;"><p style="font-family: Georgia; font-size: 28px; margin: 0px 0px 16px; font-stretch: normal; line-height: normal;">${name.elements[0].text}&nbsp;</p>`
+const ammendTopLevelTask = (name, html) => {
+  return html += `<body style="margin: 1.5em; word-wrap: break-word; -webkit-nbsp-mode: space; line-break: after-white-space;"><p style="font-family: Georgia; font-size: 28px; margin: 0px 0px 16px; font-stretch: normal; line-height: normal;">${name}&nbsp;</p>`
 }
 const ammendSubTasks = () => { }
 const ammendTaskFiles = () => { }
-const closeTask = () => { html += '</body>' }
+const closeTask = html => html += '</body>'
 
 const constructHTML = () => {
-  ammendHeader()
-
   let firstDone = false
   topLevelTasks.forEach(e => {
+    // temporary
     if (firstDone === true) return
     firstDone = true
-    ammendTopLevelTask(e)
-    closeTask()
-  });
 
-  html += '</html>'
-  fs.writeFile('constructed.html', html, (err) => {
-    if (err) throw err;
-    console.log('Yerrrrr!');
-  })
+    let html = ''
+    const name = e.elements.find(e => e.name === 'name' && e.elements.length === 1).elements[0].text
+    html = ammendHeader(html)
+    html = ammendTopLevelTask(name, html)
+    html = closeTask(html)
+    fs.writeFile(`${name}.html`, html, (err) => {
+      if (err) throw err;
+      console.log('Yerrrrr!');
+    })
+  });
 }
 
 constructHTML()
