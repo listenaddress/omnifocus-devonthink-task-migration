50210757e81b8badb23ed4665ba79b865e26f251
listenaddress <tbobrien612@gmail.com>
2021-09-11 07:53:41 -0400

    Fix migration script for Mike's cpu

--- alternate-migration.sh
+++ alternate-migration.sh
@@ -9,6 +9,9 @@ mkdir Files
 mkdir Projects
 mkdir OmniFocusContent
 unzip OmniFocus.ofocus/*.zip -d OmniFocusContent
-cp OmniFocusContent/data/*/* Files
+
+for file in `ls OmniFocus.ofocus/data/*.zip`; do
+unzip ${file} -d Files
+done
 
 node --trace-uncaught construct-html.js
